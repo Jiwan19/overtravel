a5a3609ad0e359c1f265ec3b315579cebf71f086
Andre <andrepith@yahoo.co.uk>
2018-03-19 16:39:03 +0700

    Completed desktop and mobile header style

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,32 +1,35 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create();
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
 
-    // to open server
-    browserSync.init({
-         server: {
-             baseDir: "app"
-         }
-    });
-    
-    // 1 param is file to watch
-    // 2 param is the what happen when the task run
-    watch('./app/index.html', function () {
-        browserSync.reload();
-    });
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-    watch('./app/assets/styles/**/*.css', function () {
-        gulp.start('cssInject');
-    });
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
 
 });
 
-// 1 param is task name
-// 2 param is dependencies task which the first to start before running task
-// 3 param is what happen when the task run
 gulp.task('cssInject', ['styles'], function() {
-    return gulp.src('./app/temp/styles/styles.css')
-        .pipe(browserSync.stream());
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
